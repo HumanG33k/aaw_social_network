fe7592d4605ed04e01e50aeaff5f036fb9f4abee
Nathanaël Villemin <nathanael.villemin@etu.univ-poitiers.fr>
2015-12-08 18:28:48 +0100

    Beginning the work on UsersEntity.

--- AAW_Project/src/java/dao/UsersEntity.java
+++ AAW_Project/src/java/dao/UsersEntity.java
@@ -6,10 +6,19 @@
 package dao;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 /**
  *
@@ -18,10 +27,29 @@ import javax.persistence.Id;
 @Entity
 public class UsersEntity implements Serializable {
     private static final long serialVersionUID = 1L;
+    private static final String salt = "uflil5erk4fcb";
+    private StandardPasswordEncoder encoder = new StandardPasswordEncoder(this.salt);
+    
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @Column
+    private String name;
+    @Column
+    private String password;
+    @JoinTable(
+        name="users_users",
+        joinColumns=@JoinColumn(name="user_id"),
+        inverseJoinColumns=@JoinColumn(name="user_friend_id")
+    )
+    @ManyToMany
+    private List<UsersEntity> friends = new ArrayList<>();
 
+    public UsersEntity(String name, String password) {
+        this.name = name;
+        this.password = this.encoder.encode(password);
+    }
+    
     public Long getId() {
         return id;
     }
